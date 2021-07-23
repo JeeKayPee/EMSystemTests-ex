81690190ecc82f07d2499094b705de684a3467de
Gaurav Pandey <45666264+JeeKayPee@users.noreply.github.com>
2021-07-23 20:29:25 +0530

    Update ExpenseManagerSystemTest.java

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -37,10 +37,10 @@ public class ExpenseManagerSystemTest {
 		
 		
 		//Buggy Code
-// 		String expectedTitle = "Hello Page";
+ 		String expectedTitle = "Hello Page";
 		
 		//Correct Code
-		String expectedTitle = "Login page";
+		//String expectedTitle = "Login page";
 		
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals(expectedTitle, actualTitle);
@@ -57,7 +57,7 @@ public class ExpenseManagerSystemTest {
         	BasicCredentials cred = new BasicCredentials("gaurav.ponds@gmail.com", "admin.123");
 			
 			//You need to enter your JIRA machine IP address with port 8080 in below line
-        	JiraClient jira = new JiraClient("3.239.2.203:8080", cred);
+        	JiraClient jira = new JiraClient("http://3.19.221.108:8080", cred);
 			
 			//You need to enter your JIRA project key in below line
         	Issue issueName = jira.createIssue("AUT", "Bug").field(Field.SUMMARY, result.getMethod().getMethodName() +"is failed due to: "+ result.getThrowable().toString()).field(Field.DESCRIPTION, "get the description").execute();
